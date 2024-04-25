79793b8e9a4df1f13cd78afae3009b045336985b
selligre <selligre@gmail.com>
2024-04-25 12:06:12 +0200

    removed AnnounceTag cause useless according to Louis + reformated dto files

--- fimafeng-select-client/src/main/java/edu/cgl/sirius/client/MainSelectAnnouncesLocation.java
+++ fimafeng-select-client/src/main/java/edu/cgl/sirius/client/MainSelectAnnouncesLocation.java
@@ -62,10 +62,13 @@ public class MainSelectAnnouncesLocation {
             final AsciiTable asciiTable = new AsciiTable();
             for (final Announce announce : announces.getAnnounces()) {
                 asciiTable.addRule();
-                asciiTable.addRow(announce.getAnnounce_id(), announce.getRef_author_id(), announce.getPublication_date(), 
-                                    announce.getStatus(), announce.getType(), announce.getTitle(), announce.getDescription(), announce.getDate_time_start(), 
-                                    announce.getDuration(), announce.getDate_time_end(), announce.getIs_recurrent(), announce.getSlots_number(), 
-                                    announce.getSlots_available(), announce.getPrice(), announce.getRef_location_id());
+                asciiTable.addRow(announce.getAnnounce_id(), announce.getRef_author_id(),
+                        announce.getPublication_date(),
+                        announce.getStatus(), announce.getType(), announce.getTitle(), announce.getDescription(),
+                        announce.getDate_time_start(),
+                        announce.getDuration(), announce.getDate_time_end(), announce.getIs_recurrent(),
+                        announce.getSlots_number(),
+                        announce.getSlots_available(), announce.getPrice(), announce.getRef_location_id());
                 // sBuilder.append(User.getfirst_name() + "; " + User.getName() + "; " +
                 // User.getGroup() + "\n");
             }
--- fimafeng-select-client/src/main/java/edu/cgl/sirius/client/SelectAllAnnouncesTagClientRequest.java
+++ fimafeng-select-client/src/main/java/edu/cgl/sirius/client/SelectAllAnnouncesTagClientRequest.java
@@ -4,24 +4,24 @@ import java.io.IOException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import edu.cgl.sirius.business.dto.AnnounceTag;
-import edu.cgl.sirius.business.dto.AnnouncesTag;
+import edu.cgl.sirius.business.dto.Announce;
+import edu.cgl.sirius.business.dto.Announces;
 import edu.cgl.sirius.client.commons.ClientRequest;
 import edu.cgl.sirius.client.commons.NetworkConfig;
 import edu.cgl.sirius.commons.Request;
 
-public class SelectAllAnnouncesTagClientRequest extends ClientRequest<Object, AnnouncesTag> {
+public class SelectAllAnnouncesTagClientRequest extends ClientRequest<Object, Announces> {
 
     public SelectAllAnnouncesTagClientRequest(
-            NetworkConfig networkConfig, int myBirthDate, Request request, AnnounceTag info, byte[] bytes)
+            NetworkConfig networkConfig, int myBirthDate, Request request, Announce info, byte[] bytes)
             throws IOException {
         super(networkConfig, myBirthDate, request, info, bytes);
     }
 
     @Override
-    public AnnouncesTag readResult(String body) throws IOException {
+    public Announces readResult(String body) throws IOException {
         final ObjectMapper mapper = new ObjectMapper();
-        final AnnouncesTag AnnouncesTag = mapper.readValue(body, AnnouncesTag.class);
-        return AnnouncesTag;
+        final Announces AnnouncesLocation = mapper.readValue(body, Announces.class);
+        return AnnouncesLocation;
     }
 }
