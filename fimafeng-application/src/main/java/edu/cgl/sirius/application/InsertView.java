9082dbc48f230baf75cefe23bf17ff47892a6f7a
BlooSkyd <clment2402@gmail.com>
2024-04-25 10:19:36 +0200

    essaie visuel pour l'insertion

---
+++ fimafeng-application/src/main/java/edu/cgl/sirius/application/InsertView.java
@@ -0,0 +1,52 @@
+package edu.cgl.sirius.application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class InsertView {
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    void handleAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleActivities(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAroundMe(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCreate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMaterials(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleServices(ActionEvent event) {
+
+    }
+
+}
--- fimafeng-application/src/main/java/edu/cgl/sirius/application/MainApp.java
+++ fimafeng-application/src/main/java/edu/cgl/sirius/application/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ApplicationFXML.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/InsertView.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         stage.setTitle("Ville partagée");
