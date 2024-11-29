622018519d6418c1474a50ee51df3fe187ba6bd2
BlooSkyd <clment2402@gmail.com>
2024-11-29 14:44:33 +0100

    update de l'ip des machines sur localhost + nouvelles tentatives jenkins

--- proto-front/src/constants/back.js
+++ proto-front/src/constants/back.js
@@ -1,4 +1,4 @@
-export const LOCAL_HOST = 'http://172.31.250.67:8080';
+export const LOCAL_HOST = 'http://localhost:8080';
 export const LOCAL_HOST_ANNOUNCE = LOCAL_HOST + '/announce/';
 export const GET_ANNOUNCES = LOCAL_HOST_ANNOUNCE + 'all';
 export const ADD_ANNOUNCE = LOCAL_HOST_ANNOUNCE + 'add';
