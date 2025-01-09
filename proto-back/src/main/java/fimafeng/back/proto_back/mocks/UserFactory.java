229ecec6fa831dd73193f9bb69159e8ebac82080
Clement <clment2402@gmail.com>
2025-01-09 23:56:11 +0100

    Completion de la generation de l'utilisateur à partir des données de la table

--- proto-back/src/main/java/fimafeng/back/proto_back/mocks/UserFactory.java
+++ proto-back/src/main/java/fimafeng/back/proto_back/mocks/UserFactory.java
@@ -1,36 +1,45 @@
 package fimafeng.back.proto_back.mocks;
 
 import com.github.javafaker.Faker;
+import fimafeng.back.proto_back.models.District;
 import fimafeng.back.proto_back.models.User;
+import fimafeng.back.proto_back.services.DistrictService;
 
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.logging.Logger;
-
 
 public class UserFactory extends User {
 
-    Logger LOGGER = Logger.getLogger(UserFactory.class.getName());
+    private final Logger LOGGER = Logger.getLogger(UserFactory.class.getName());
 
-    Faker FAKER = new Faker(new Locale("fr-FR"));
+    private final Faker FAKER = new Faker(new Locale("fr-FR"));
 
-    List<Integer> distritsIdsList;
+    List<Integer> districtsListWeighted;
 
     public UserFactory() {
+        districtsListWeighted = new ArrayList<>();
+
+        // Getting districts data from DB
+        DistrictService districtService = new DistrictService();
+        List<District> districtsList = districtService.findAll();
+
+        // Generating a list weighted from district data
+        for (District district : districtsList) {
+            for (int i = 0 ; i < district.getPopulationPercentile() ; i++) {
+                districtsListWeighted.add(district.getId());
+            }
+        }
 
     }
 
 
-    public User create() {
+    public User generate() {
         LOGGER.info("Creating user");
         User user = new User();
         user.setFirstName(FAKER.name().firstName());
         user.setLastName(FAKER.name().lastName());
         user.setEmail(FAKER.internet().emailAddress());
-
-        // definition temporaire en attendant d'avoir des données en base
-        // TODO: definir à partir de la table `location` quand elle aura été populée
-        user.setDistrict(FAKER.number().randomDigit());
+        user.setDistrict(districtsListWeighted.get(FAKER.random().nextInt(0, districtsListWeighted.size())));
         LOGGER.info("User created: " + user.toString());
         return user;
 
