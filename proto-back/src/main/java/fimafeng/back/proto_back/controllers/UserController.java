8f0cfa82f22c50c87dd7000c601a37126542d117
Clement <clment2402@gmail.com>
2025-01-10 00:05:11 +0100

    usage du service, prêt pour test postman

--- proto-back/src/main/java/fimafeng/back/proto_back/controllers/UserController.java
+++ proto-back/src/main/java/fimafeng/back/proto_back/controllers/UserController.java
@@ -3,6 +3,7 @@ package fimafeng.back.proto_back.controllers;
 import fimafeng.back.proto_back.mocks.UserFactory;
 import fimafeng.back.proto_back.models.User;
 import fimafeng.back.proto_back.repositories.UserRepository;
+import fimafeng.back.proto_back.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
     private final UserFactory userFactory = new UserFactory();
 
     @GetMapping("/generate")
     public ResponseEntity<User> generate() {
-        User generatedUser = userFactory.generate();
+        User generatedUser = userService.save(userFactory.generate());
         return new ResponseEntity<>(generatedUser, HttpStatus.CREATED);
     }
 }
