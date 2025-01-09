a5772bb43465b53f0ae4da0fe3a5faa7ce45554e
Clement <clment2402@gmail.com>
2025-01-09 15:21:34 +0100

    Suppression de lambok (pb de compilation) et rajout des getters/setters en compensation

--- proto-back/src/main/java/fimafeng/back/proto_back/models/District.java
+++ proto-back/src/main/java/fimafeng/back/proto_back/models/District.java
@@ -1,23 +1,44 @@
 package fimafeng.back.proto_back.models;
 
-
-import lombok.Data;
-
 import jakarta.persistence.*;
 
-@Data
 @Entity
 @Table(name = "district")
 public class District {
 
     // Getters and setters
     @Id
-    @Column(name="id")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
     private String name;
 
+    private float populationPercentile;
 
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPopulationPercentile() {
+        return populationPercentile;
+    }
+
+    public void setPopulationPercentile(float populationPercentile) {
+        this.populationPercentile = populationPercentile;
+    }
 }
--- proto-back/src/main/java/fimafeng/back/proto_back/models/User.java
+++ proto-back/src/main/java/fimafeng/back/proto_back/models/User.java
@@ -1,30 +1,27 @@
 package fimafeng.back.proto_back.models;
 
-import lombok.Data;
-
 import jakarta.persistence.*;
 
-@Data
 @Entity
 @Table(name = "user")
 public class User {
 
     // Getters and setters
     @Id
-    @Column(name="id")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name="first_name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name="last_name")
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name="email")
+    @Column(name = "email")
     private String email;
 
-    @Column(name="ref_district")
+    @Column(name = "ref_district")
     private int district;
 
     @Override
@@ -38,4 +35,44 @@ public class User {
         return res;
     }
 
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(int district) {
+        this.district = district;
+    }
 }
