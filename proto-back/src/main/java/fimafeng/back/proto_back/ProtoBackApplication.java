18693604266e8a39da1ff78a8662bd1ba60efee6
Clement <clment2402@gmail.com>
2025-01-10 11:32:49 +0100

    Suppression des modules et du build pour optimisation du git (données récupérées par jenkins) + completion de l'objet annonce (champ ref_author_id manquant)

--- proto-back/src/main/java/fimafeng/back/proto_back/ProtoBackApplication.java
+++ proto-back/src/main/java/fimafeng/back/proto_back/ProtoBackApplication.java
@@ -2,8 +2,12 @@ package fimafeng.back.proto_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ProtoBackApplication {
 
 	public static void main(String[] args) {
