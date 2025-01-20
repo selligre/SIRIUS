5e643f4bace3eafffd0dddc63f9b6636807f277f
selligre <selligre@gmail.com>
2025-01-20 11:50:37 +0100

    Removed an annotation that caused the app to crash on compilation

--- proto-back/src/main/java/fimafeng/back/proto_back/ProtoBackApplication.java
+++ proto-back/src/main/java/fimafeng/back/proto_back/ProtoBackApplication.java
@@ -2,12 +2,9 @@ package fimafeng.back.proto_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class ProtoBackApplication {
 
     public static void main(String[] args) {
