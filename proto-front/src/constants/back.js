d8de68ba485ed1ce92d53847b7d7d1726f5e458c
Clement Taurand <clment2402@gmail.com>
2025-01-23 23:45:48 +0100

    hotfix: setting back localhost address

--- proto-front/src/constants/back.js
+++ proto-front/src/constants/back.js
@@ -1,4 +1,4 @@
-export const LOCAL_HOST = 'http://172.31.240.24:8080';
+export const LOCAL_HOST = 'http://localhost:8080';
 export const LOCAL_HOST_ANNOUNCE = LOCAL_HOST + '/announce';
 export const GET_ANNOUNCES = LOCAL_HOST_ANNOUNCE + '/all';
 export const ADD_ANNOUNCE = LOCAL_HOST_ANNOUNCE + '/add';
