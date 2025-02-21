7354682c415fbae960d0c722ef70f83b7b686c25
louis <louistran29112003@gmail.com>
2025-02-21 16:26:07 +0100

    structure changement

--- fimafeng-back/src/main/java/fimafeng/back/fimafeng_back/FimafengBackApplication.java
+++ fimafeng-back/src/main/java/fimafeng/back/fimafeng_back/FimafengBackApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ProtoBackApplication {
+public class FimafengBackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProtoBackApplication.class, args);
+        SpringApplication.run(FimafengBackApplication.class, args);
     }
 
 }
--- fimafeng-back/src/test/java/fimafeng/back/proto_back/FimafengBackApplicationTests.java
+++ fimafeng-back/src/test/java/fimafeng/back/proto_back/FimafengBackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProtoBackApplicationTests {
+class FimafengBackApplicationTests {
 
 	@Test
 	void contextLoads() {
