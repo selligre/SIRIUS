146340955aaff8a9a7591d63f014ebdd5f4593a0
selligre <92374805+selligre@users.noreply.github.com>
2026-02-26 00:25:24 +0100

    Limit log output to last 10 AuthSuccess entries

--- scripts/logs-login.ps1
+++ scripts/logs-login.ps1
@@ -2,4 +2,4 @@ param(
     [Parameter(Mandatory = $true)]
     [int]$port
 )
-ssh fimafeng@172.31.249.140 -p $port "cat oauth2-proxy.log | grep AuthSuccess"
+ssh fimafeng@172.31.249.140 -p $port "cat oauth2-proxy.log | grep AuthSuccess | tail -n 10"
