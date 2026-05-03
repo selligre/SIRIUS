0b61c7b6a8cc45b66afdd7f168892dba313e6bc3
selligre <selligre@gmail.com>
2026-05-03 19:37:07 +0200

    update public ip after vsphere restart

--- scripts/docker-kill-all.ps1
+++ scripts/docker-kill-all.ps1
@@ -2,4 +2,4 @@ param(
     [Parameter(Mandatory = $true)]
     [int]$port
 )
-ssh fimafeng@172.31.249.140 -p $port "sudo -S docker ps -aq | sudo -S xargs docker kill"
+ssh fimafeng@172.31.250.155 -p $port "sudo -S docker ps -aq | sudo -S xargs docker kill"
--- scripts/ssh.ps1
+++ scripts/ssh.ps1
@@ -2,4 +2,4 @@ param(
     [Parameter(Mandatory = $true)]
     [int]$port
 )
-ssh fimafeng@172.31.249.140 -p $port
+ssh fimafeng@172.31.250.155 -p $port
