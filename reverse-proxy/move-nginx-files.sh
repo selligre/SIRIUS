bd1bee6aa2e66842e8c0649243dd2ad9f4995e4d
Clement Taurand <clment2402@gmail.com>
2026-05-20 15:37:07 +0200

    backup nginx conf and add cp script

---
+++ reverse-proxy/move-nginx-files.sh
@@ -0,0 +1,6 @@
+# Déplacement des fichiers
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.prev
+cp ./nginx.conf /etc/nginx/nginx.conf
+
+cp /etc/nginx/sites-enabled/app /etc/nginx/sites-enabled/app.prev
+cp ./app /etc/nginx/sites-enabled/app
