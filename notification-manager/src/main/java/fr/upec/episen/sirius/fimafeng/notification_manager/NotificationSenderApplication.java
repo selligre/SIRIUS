374175d9575d7ca9bab0b234c907ad46d4519499
Clement Taurand <clment2402@gmail.com>
2026-05-21 13:56:56 +0200

    Create basic notif sender maven project

---
+++ notification-manager/src/main/java/fr/upec/episen/sirius/fimafeng/notification_manager/NotificationSenderApplication.java
@@ -0,0 +1,26 @@
+package fr.upec.episen.sirius.fimafeng.notification_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = {
+    "fr.upec.episen.sirius.fimafeng.commons.models",
+    "fr.upec.episen.sirius.fimafeng.notifications"
+})
+@EnableJpaRepositories(basePackages = {
+    "fr.upec.episen.sirius.fimafeng.notifications.repositories"
+})
+@ComponentScan(basePackages = {
+    "fr.upec.episen.sirius.fimafeng.notifications",
+    "fr.upec.episen.sirius.fimafeng.commons"
+})
+public class NotificationSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationSenderApplication.class, args);
+    }
+}
